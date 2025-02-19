eacc4d93629f391723f3fe3ceea5c708f81344d9
Yuldashev <ikrom921@gmail.com>
2025-02-19 17:54:26 +0500

    Initial Commit

---
+++ Snacks_Shop_App/Snacks_Shop_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snacks_Shop_AppApp.swift
+//  Snacks_Shop_App
+//
+//  Created by Yuldashev Fazliddin on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Snacks_Shop_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
