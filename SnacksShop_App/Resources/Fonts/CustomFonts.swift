104a054500d2ec9abaa02485e0fae9cd898ab8b8
Yuldashev <ikrom921@gmail.com>
2025-02-20 09:41:28 +0500

    Assets add font, color, icon

---
+++ SnacksShop_App/Resources/Fonts/CustomFonts.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Font {
+  static func jakartaRegular(size: CGFloat) -> Font {
+    return Font.custom("PlusJakartaSans-Regular", size: size)
+  }
+  
+  static func jakartaBold(size: CGFloat) -> Font {
+    return Font.custom("PlusJakartaSans-Bold", size: size)
+  }
+  
+  static func jakartaMedium(size: CGFloat) -> Font {
+    return Font.custom("PlusJakartaSans-Medium", size: size)
+  }
+}
