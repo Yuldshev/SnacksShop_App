4c80a3b3c3b09a25a34bc0aeefc8caceae7cdc2d
Yuldashev <ikrom921@gmail.com>
2025-02-24 09:27:18 +0500

    [Add] HeaderView

---
+++ SnacksShop_App/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  SnacksShop_App
+//
+//  Created by Yuldashev Fazliddin on 24/02/25.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
